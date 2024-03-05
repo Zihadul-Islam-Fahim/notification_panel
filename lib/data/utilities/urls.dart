9754fb8c31aa57538923b428eb436ca81a6abf84
Zihadu-Islam-Fahim <fahimzihad@gmail.com>
2024-03-05 22:54:28 +0600

    initial commit

--- lib/data/utilities/urls.dart
+++ lib/data/utilities/urls.dart
@@ -3,5 +3,5 @@ class Urls{
   static String getNotification(int size) => '$baseUrl/get_notification?page=1&pageSize=$size';
   static String updateNotification = '$baseUrl/update_notification_status';
 
-  static String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzA5NDczOTI3LCJleHAiOjE3MDk1NjAzMjd9.VXBCSlvoN4MUz8Tkkw7pLZU7DHD9OgMNnFxmtEKe5Hk';
+  static String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzA5NjU3NTYzLCJleHAiOjE3MDk3NDM5NjN9.pipj9aEYdtfYas8CEuAgMR0YRoBJxtrAHISEwXBkLC8';
 }
